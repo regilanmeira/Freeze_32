30b4c49ab64059bf0bc4a98c93d8be76e70e0f59
regilanmeira <regilan@hotmail.com>
2024-10-15 11:46:47 -0300

    Codificado a criação de usuarios no BDA

--- usuario_cadastro_salvar.php
+++ usuario_cadastro_salvar.php
@@ -9,6 +9,38 @@
 <!-- INCLUSÃO DO TOPO DA PÁGINA -->    
 <?php include "header_admin.php" ?>
 
+<?php 
+//1º PASSO: INCLUSÃO DAS FUNÇÕES DE ACESSO A DADOS
+include "conexao_bd.php";
+
+//2º PASSO: CAPTURAR O LOGIN E SENHA DIGITADO PELO USUÁRIO
+$login_usuario = $_POST["txtLoginUsuario"];
+$senha_usuario = $_POST["txtSenhaUsuario"];
+
+$hash = password_hash($senha_usuario,1);
+
+//3º PASSO: COMANDO SQL
+$sql = "INSERT INTO usuario(login_usuario,senha_usuario) ";
+$sql .= " VALUES('$login_usuario','$hash') ";
+
+//4º PASSO: Executar no BDA
+if (executarComando($sql))
+{
+    echo "<h1>Usuário adicionado!</h1>";
+}
+else
+{
+    echo "<h1>Usuário não cadastrado</h1>";
+}
+
+
+
+
+?>
+
+
+
+
 <!-- INCLUSÃO DO RODAPE -->
 <?php include "footer_admin.php" ?>
 </body>
