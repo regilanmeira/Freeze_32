72fbcb9c81c358f3f9ecde55ac4b24080b7c380b
regilanmeira <regilan@hotmail.com>
2024-10-15 10:37:14 -0300

    Criado a página usuario salvar

---
+++ usuario_cadastro_salvar.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include "referencias.php"?>
+</head>
+<body>
+<!-- INCLUSÃO DO TOPO DA PÁGINA -->    
+<?php include "header_admin.php" ?>
+
+<!-- INCLUSÃO DO RODAPE -->
+<?php include "footer_admin.php" ?>
+</body>
+</html>
